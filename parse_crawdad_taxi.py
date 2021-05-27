af595694c553303bc7d4f47956661266fb286044
Ubuntu <onefog@onefog-node-2.ki4c5amb05de1fusnpr0gnfyaf.bx.internal.cloudapp.net>
2021-05-27 04:04:09 +0000

    Transformation into CARLA coordiantes

--- parse_crawdad_taxi.py
+++ parse_crawdad_taxi.py
@@ -6,6 +6,21 @@ from geopy.distance import geodesic
 import geopy.distance
 import math, numpy as np
 
+SF_MIN =  (37.675,-122.48)
+SF_MAX =  (37.825,-122.35)
+CARLA_MIN = (-0.0018703476525843143, -0.0013416497968137264)
+CARLA_MAX = (0.0018795838113874197, 0.002198888687416911)
+
+def transform_lat(lat):
+    slope = (CARLA_MAX[0] - CARLA_MIN[0])/(SF_MAX[0] - SF_MIN[0])
+    intersection = (SF_MAX[0] *CARLA_MIN[0] - CARLA_MAX[0] * SF_MIN[0]) / (SF_MAX[0] - SF_MIN[0])
+    return lat * slope + intersection
+
+def transform_lng(lng):
+    slope = (CARLA_MAX[1] - CARLA_MIN[1])/(SF_MAX[1] - SF_MIN[1])
+    intersection = (SF_MAX[1] *CARLA_MIN[1] - CARLA_MAX[1] * SF_MIN[1]) / (SF_MAX[1] - SF_MIN[1])
+    return lng * slope + intersection
+
 def get_bearing(lat1,lon1,lat2,lon2):
     dLon = lon2 - lon1;
     y = math.sin(dLon) * math.cos(lat2);
@@ -13,7 +28,6 @@ def get_bearing(lat1,lon1,lat2,lon2):
     brng = np.rad2deg(math.atan2(y, x));
     if brng < 0: brng+= 360
     return brng
-
 
 def check_operation(operation):
     if operation == "limits" or operation == "generate":
@@ -37,6 +51,8 @@ def main(directory,operation, min_time=None ,max_time=None,time_delta=None):
                     input_vals.append([lat,lng,unixtimestamp])
                 input_vals.sort(key=lambda x: x[2])
                 prev = None
+              
+                last_good_loc = None
                 for row in input_vals:
                     lat = row[0]
                     lng = row[1]
@@ -45,6 +61,14 @@ def main(directory,operation, min_time=None ,max_time=None,time_delta=None):
                         continue
                     elif max_time and unixtimestamp > max_time:
                         continue
+
+                    if lat < SF_MIN[0] or lat > SF_MAX[0] or lng < SF_MIN[1] or lng > SF_MAX[1]:
+                      if last_good_loc == None:
+                        continue
+                      else:
+                        (lat, lng) = last_good_loc
+                    last_good_loc = (lat, lng)
+
                     if name not in data:
                         data[name]=[]
                     if prev and time_delta:
@@ -61,9 +85,12 @@ def main(directory,operation, min_time=None ,max_time=None,time_delta=None):
                         for i in range(num):
                             nextpoint = each_distance.destination((prev[0],prev[1]), bearing=bearing)
                             nextval = (nextpoint.latitude, nextpoint.longitude, prev[2]+time_delta)
-                            data[name].append((nextval[0],nextval[1],float(nextval[2])/1000.0))
+                            data[name].append((transform_lat(nextval[0]),transform_lng(nextval[1]),float(nextval[2])/1000.0))
                             prev = nextval 
-                    data[name].append((lat,lng,float(unixtimestamp)/1000.0))
+
+                    trans_lat = transform_lat(lat)
+                    trans_lng = transform_lng(lng)
+                    data[name].append((trans_lat,trans_lng,float(unixtimestamp)/1000.0))
                     prev = (lat,lng,unixtimestamp)
     if operation == "limits":
         min_time = []
